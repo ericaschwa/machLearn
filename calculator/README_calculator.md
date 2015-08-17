3c350f09ec217b1d2ced6afa521ab1b9d8d65de3
Erica Schwartz <erica.schwartz.4@gmail.com>
2015-08-17 09:20:30 -0400

    fixed calculator bugs

---
+++ calculator/README_calculator.md
@@ -0,0 +1,7 @@
+This program does not exhibit machine learning persay. Instead, it acts as a calculator. However, possible extensions to this
+program do include implementing a machine learning aspect; perhaps the user can introduce new symbols such as $ and the computer
+must learn what these symbols mean
+
+To run: python "calcluator.py"
+
+As it currently stands, the program passes all tests. It respects order of operations (PEMDAS), and can also do mod and handle floats and negative numbers.
--- calculator/calculator.py
+++ calculator/calculator.py
@@ -6,25 +6,23 @@
 #																														#
 #########################################################################################################################
 
+import math
+
 ###############################################################################
 #							 FUNCTION DEFINITIONS							  #
 ###############################################################################
 
-
-#TODO: PEMDAS
-
-# global variable representing the stack containing various parts of the expression, ordered according to parentheses
-expressionStack = []
-
 # carries out function of calculator for one-line calculation, taking parentheses into account.
 # Calculations can build upon previous calculations.
-def calculate (expression):
+def calculate (expression, expressionStack):
 	parenthStack = []
+	if (not(expression[0] == '^' or expression[0] == '*' or expression[0] == '/' or expression[0] == '%' or expression[0] == '+' or expression[0] == '-')):
+		expressionStack = [] # start fresh with new stack
 	for val in expression:
-		if (val == "("):
+		if (val == "("): # start of a parenthesized statement
 			parenthStack.append(val)
 			expressionStack.append("")
-		elif (val == ")"):
+		elif (val == ")"): # end of a parenthesized statement; evaluate the statement and push the result back to the stack
 			if (len(parenthStack) > 0):
 				parenthStack.pop()
 				prev = expressionStack.pop()
@@ -37,8 +35,8 @@ def calculate (expression):
 				expressionStack.append(top)
 			else:
 				print "Error: parentheses do not match up"
-				return 1
-		else:
+				return []
+		else: # non-parenthesis value added; push it to the stack
 			if (len(expressionStack) > 0):
 				top = expressionStack.pop()
 			else:
@@ -47,7 +45,7 @@ def calculate (expression):
 			expressionStack.append(top)
 	if (len(parenthStack) != 0):
 		print "Error: parentheses do not match up"
-		return 1
+		return []
 	if (len(expressionStack) > 0):
 		top = expressionStack.pop()
 	else:
@@ -55,33 +53,62 @@ def calculate (expression):
 	top = evaluate(top)
 	expressionStack.append(top + " ")
 	print top
+	return expressionStack
 
 # evaluates an expression within a parentheses set
 def evaluate (expression):
 	parts = expression.split(" ")
 	if (len(parts) < 3):
 		return expression
+
+	# E in PEMDAS
+	i = 0
+	while (i < len(parts)):
+		if (parts[i] == '^'):
+			parts[i - 1] = str(math.pow(int(float(parts[i - 1])), int(float(parts[i + 1]))))
+			parts.pop(i)
+			parts.pop(i)
+		i += 1
+
+	# M and D in PEMDAS (and mod)
+	i = 0
+	while (i < len(parts)):
+		if (parts[i] == '*'):
+			parts[i - 1] = str(float(parts[i - 1]) * float(parts[i + 1]))
+			parts.pop(i)
+			parts.pop(i)
+		elif (parts[i] == '/'):
+			parts[i - 1] = str(float(parts[i - 1]) / float(parts[i + 1]))
+			parts.pop(i)
+			parts.pop(i)
+		elif (parts[i] == '%'):
+			parts[i - 1] = str(float(parts[i - 1]) % float(parts[i + 1]))
+			parts.pop(i)
+			parts.pop(i)
+		i += 1
+
+	# A and S in PEMDAS
 	start = parts[0]
-	expIndex = 1
-	endIndex = 2
-	while (endIndex < len(parts) and expIndex < len(parts)):
-		exp = parts[expIndex]
-		end = parts[endIndex]
-		start = f(start, exp, end)
-		expIndex += 2
-		endIndex += 2
-	return start
+ 	expIndex = 1
+ 	endIndex = 2
+ 	while (endIndex < len(parts) and expIndex < len(parts)):
+ 		exp = parts[expIndex]
+ 		end = parts[endIndex]
+ 		start = f(start, exp, end)
+ 		expIndex += 2
+ 		endIndex += 2
+ 	return start
 
 # applies whatever operation user wanted (+ , -, *, /, ^, or %) to the numbers entered and returns the result
 def f (a,exp,b):
-	return {
+ 	return {
 		'+': str(float(a) + float(b)), #third
 		'-': str(float(a) - float(b)), #third
 		'*': str(float(a) * float(b)), #second
 		'/': str(float(a) / float(b)), #second
-		'^': str(pow(float(a), float(b))), #first
+		'^': str(math.pow(int(float(a)), int(float(b)))), #first
 		'%': str(float(a) % float(b)), #second
-	}[exp]
+ 	}[exp]
 
 ###############################################################################
 #									MAIN									  #
@@ -89,6 +116,7 @@ def f (a,exp,b):
 
 # Get user-inputted math expression
 user_input = raw_input()
+expressionStack = []
 while (user_input != "Q"):
-	calculate(user_input)
+	expressionStack = calculate(user_input, expressionStack)
 	user_input = raw_input()

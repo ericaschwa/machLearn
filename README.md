ccc101798f0e11bdd92de432d6416941f9d598db
Erica Schwartz <erica.schwartz.4@gmail.com>
2015-08-18 17:16:24 -0400

    starting on election predictor by getting data

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ refer to those for information relating to the program and its compilation.
 
 I made this repository as sort of an experiment- it is my first repository on
 my personal GitHub account, and I'm using it to explore one topic that I'm
-very interested in: machine learning.
+very interested in: machine learning. (as well as a few other topics)
--- election_predictor/elections.py
+++ election_predictor/elections.py
@@ -1,1 +1,80 @@
-# http://www.anthonydebarros.com/2013/02/05/get-json-from-excel-using-python-xlrd/
+#########################################################################################################################
+#																														#
+# 	This program exhibits machine learning by reading in various pieces of data by state and year, and based on 		#
+#	correct examples, predicts the outcome of the closest upcoming presidential election to that date in that state.	#
+#																														#
+#########################################################################################################################
+
+import xlrd
+from collections import OrderedDict
+import json
+
+###############################################################################
+#							 FUNCTION DEFINITIONS							  #
+###############################################################################
+
+# get the election results by state and year (0 means republican; 1 means democrat)
+def get_election_data():
+	# Open the workbook
+	wb = xlrd.open_workbook('elections.xlsx')
+	sh = wb.sheet_by_index(0)
+
+	elections_list = []
+
+	# Iterate through each row in worksheet and fetch values into dict
+	# add four years for each election result, since elections happen
+	# every four years
+	for rownum in range(1, sh.nrows):
+	    elections = OrderedDict()
+	    row_values = sh.row_values(rownum)
+	    elections['state'] = row_values[0]
+	    elections['year'] = int(row_values[1] - 3)
+	    elections['result'] = int(row_values[2])
+	    elections_list.append(elections)
+
+	    elections = OrderedDict()
+	    row_values = sh.row_values(rownum)
+	    elections['state'] = row_values[0]
+	    elections['year'] = int(row_values[1] - 2)
+	    elections['result'] = int(row_values[2])
+	    elections_list.append(elections)
+
+	    elections = OrderedDict()
+	    row_values = sh.row_values(rownum)
+	    elections['state'] = row_values[0]
+	    elections['year'] = int(row_values[1] - 1)
+	    elections['result'] = int(row_values[2])
+	    elections_list.append(elections)
+
+	    elections = OrderedDict()
+	    row_values = sh.row_values(rownum)
+	    elections['state'] = row_values[0]
+	    elections['year'] = int(row_values[1])
+	    elections['result'] = int(row_values[2])
+	    elections_list.append(elections)
+	 
+	# Serialize the list of dicts to JSON
+	j = json.dumps(elections_list)
+	return j
+
+# get crime data by state and year
+def get_crime_data():
+	#open file containing sample data json
+	with open('crimedata.json', 'r') as f: # to use file that includes the test data (accuracy: 0.962962962963)
+	     read_data = f.read()
+	     crimedata = json.loads(read_data)
+	f.closed
+	return crimedata
+
+def combine_structures(elections, crimedata):
+	return 0
+
+
+###############################################################################
+#									MAIN									  #
+###############################################################################
+ 
+elections = get_election_data()
+crimedata = get_crime_data()
+data = combine_structures(elections, crimedata)
+print data

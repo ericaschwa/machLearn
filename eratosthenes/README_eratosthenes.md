0b637ac9d8f8fe5b7f4c7bf8e6a62dad43a5653b
Erica Schwartz <erica.schwartz.4@gmail.com>
2015-08-30 08:18:33 -0400

    started on algorithm

--- eratosthenes/README_eratosthenes.md
+++ eratosthenes/README_eratosthenes.md
@@ -1,6 +1,7 @@
 This program does not exhibit machine learning persay. Instead, it lists the
 prime numbers up to a given number using the Seive of Eratosthenes algorithm.
 The user inputs this stopping point number on the command line. 
+User cannot input a negative number.
 
 To run: python "eratosthenes.py"
 
--- eratosthenes/eratosthenes.py
+++ eratosthenes/eratosthenes.py
@@ -3,6 +3,7 @@
 # 	This program lists the prime numbers up to a given number using			  #
 #	the Seive of Eratosthenes algorithm. The user inputs this stopping		  #
 #	point number on the command line. 										  #
+#	User cannot input a negative number. 								  	  #
 #																			  #
 ###############################################################################
 
@@ -20,3 +21,5 @@ import sys
 ###############################################################################
 
 
+number = float(sys.argv[1])
+sqrt = math.sqrt(number)

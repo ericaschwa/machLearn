6d5297830505eb1938f6a4711dcc8fb89db30649
Erica Schwartz <erica.schwartz.4@gmail.com>
2015-09-03 14:41:53 -0400

    finished get_transaction_data

--- transactions/get_transaction_data.py
+++ transactions/get_transaction_data.py
@@ -1,10 +1,8 @@
 ###############################################################################
 #																		      #
-# 	This program reads in data structures containing crime and election       #
-#	data from JSON files and excels, combines them into one json		      #
-#	object, and prints this JSON object to a file.						      #
-#																			  #
-#	Used crime data because that was the data available to me.				  #
+# 	This program reads in data about financial transactions (id,              #
+#	subscription, amount, and date) from an excel spreadsheet, and prints     #
+#	this data in JSON form to a file.										  #
 #																			  #
 ###############################################################################
 
@@ -20,26 +18,25 @@ from collections import OrderedDict
 # source: https://www.mindsumo.com/contests/credit-card-transactions
 def make_data():
 	# Open the workbook
-	wb = xlrd.open_workbook('income.xls')
+	wb = xlrd.open_workbook('subscription_report.xls')
 	sh = wb.sheet_by_index(0)
 
-	incomeData = []
+	transactionData = []
 
 	# Iterate through each row in worksheet and fetch values into dict
-	for rownum in range(0, sh.nrows):
+	for rownum in range(1, sh.nrows):
 		row_values = sh.row_values(rownum)
-		for colnum in range(0, 24):
-			data = OrderedDict()
-			data['state'] = row_values[0]
-			data['year'] = 2013 - colnum
-			data['income'] = row_values[2 * colnum + 1]
-			data['income stderr'] = row_values[2 * colnum + 2]
-			incomeData.append(data)
+		data = OrderedDict()
+		data['id'] = row_values[0]
+		data['subscription'] = row_values[1]
+		data['amount'] = row_values[2]
+		data['data'] = row_values[3]
+		transactionData.append(data)
 	
-	#Serialize the list of dicts to JSON
-	j = json.dumps(incomeData)
-	with open('incomedata.json', 'w') as f:
-	    	f.write(j)
+	# Put the list of dicts to JSON
+	j = json.dumps(transactionData)
+	with open('data.json', 'w') as f:
+	    f.write(j)
 	f.closed
 
 
